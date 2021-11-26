7d9a10cbd8b10655ff0b803c7d4874583e33d084
ysantana <net_developer>
2021-11-26 14:56:39 -0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/cleanarchitecture_tdd_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cleanarchitecture_tdd_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/ysantana/flutter2_0"
-export "FLUTTER_APPLICATION_PATH=/Users/ysantana/Documents/ysantana/Flutter Developer/demos/CleanArchitecture-TDD-Flutter/cleanarchitecture_tdd_flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ysantana/Documents/ysantana/Flutter Developer/demos/cleanarchitecture_tdd_flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
